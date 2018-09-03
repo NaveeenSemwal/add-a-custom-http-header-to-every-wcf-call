a75846770e708292680206d1caebe1e520effc4b
naveen semwal <naveenrhtdm4ever@gmail.com>
2018-09-03 23:10:29 +0530

    Console application with WCF SOAP

---
+++ ConsoleOktaClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleOktaClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://stackoverflow.com/questions/964433/how-to-add-a-custom-http-header-to-every-wcf-call
+
+            OktaServiceReference.OktaServiceClient clientOkta = new OktaServiceReference.OktaServiceClient();
+
+            var result = clientOkta.DoWorkAsync();
+
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ SimpleServiceLib/SimpleMessageInspector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleServiceLib
+{
+    // Client message inspector  
+    public class SimpleMessageInspector : IClientMessageInspector
+    {
+        public void AfterReceiveReply(ref System.ServiceModel.Channels.Message reply, object correlationState)
+        {
+            // Implement this method to inspect/modify messages after a message  
+            // is received but prior to passing it back to the client   
+            Console.WriteLine("AfterReceiveReply called");
+        }
+
+        public object BeforeSendRequest(ref System.ServiceModel.Channels.Message request, IClientChannel channel)
+        {
+
+            HttpRequestMessageProperty httpRequestMessage;
+            object httpRequestMessageObject;
+            if (request.Properties.TryGetValue(HttpRequestMessageProperty.Name, out httpRequestMessageObject))
+            {
+                httpRequestMessage = httpRequestMessageObject as HttpRequestMessageProperty;
+                if (string.IsNullOrEmpty(httpRequestMessage.Headers["Authorization"]))
+                {
+                    httpRequestMessage.Headers["Authorization"] = "Naveen";
+                }
+            }
+            else
+            {
+                httpRequestMessage = new HttpRequestMessageProperty();
+                httpRequestMessage.Headers.Add("Authorization", "Naveen");
+                request.Properties.Add(HttpRequestMessageProperty.Name, httpRequestMessage);
+            }
+
+            return null;
+
+        }
+    }
+}
